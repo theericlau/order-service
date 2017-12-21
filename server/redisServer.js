69062a41c95a59dc1300f27a8d047efa6129ad99
Eric <theericlau@gmail.com>
2017-12-21 14:37:01 -0800

    Redis Server added

---
+++ server/redisServer.js
@@ -0,0 +1,20 @@
+const redis = require('redis');
+const Promise = require('bluebird');
+
+
+const client = redis.createClient(process);
+
+Promise.promisifyAll(redis.RedisClient.prototype);
+
+client.on('connect', function () {
+  console.log('Connected to Redis...');
+});
+
+const addCartCache = (cart) => {
+  console.log(cart);
+  return client.setAsync(cart.userid, JSON.stringify(cart.cart));
+}
+
+module.exports = {
+  addCartCache,
+}
